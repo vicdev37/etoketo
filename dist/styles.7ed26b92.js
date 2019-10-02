c703d4014f71838509fea5071321d5ee9fc3d539
Victor <justicejesus1237@gmail.com>
2019-10-02 23:23:10 +0300

    done: header & dish

--- dist/styles.7ed26b92.js
+++ dist/styles.7ed26b92.js
@@ -195,7 +195,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"normalize.css":"../node_modules/normalize.css/normalize.css","./../images/background.svg":[["background.42599997.svg","../src/images/background.svg"],"../src/images/background.svg"],"./../images/header-bg.png":[["header-bg.72cd8de1.png","../src/images/header-bg.png"],"../src/images/header-bg.png"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"normalize.css":"../node_modules/normalize.css/normalize.css","./../images/header-bg.png":[["header-bg.72cd8de1.png","../src/images/header-bg.png"],"../src/images/header-bg.png"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -223,7 +223,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54953" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52235" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
