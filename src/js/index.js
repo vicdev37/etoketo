3bcecd8a5892f5e927d89294c29c10665ebef024
pelevin-d-y <natpuot1992@gmail.com>
2019-10-06 18:36:21 +0300

    add gradient

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,20 @@
 var jquery = require("jquery");
+const bodyScrollLock = require('body-scroll-lock');
+const disableBodyScroll = bodyScrollLock.disableBodyScroll;
+const enableBodyScroll = bodyScrollLock.enableBodyScroll;
+
 window.$ = window.jQuery = jquery;
 import 'babel-polyfill';
 
 $(document).ready(() => {
   const menu = $('.menu-nav')[0]
-  const menuHeight = menu.offsetHeight
+  const menuHeight = menu.offsetHeight + 5
 
   let lastPageYOffset = pageYOffset
   let transform = 0
 
-  window.addEventListener('scroll', (evt) => {
+  const scrollHandler = (evt) => {
+    
     if ((lastPageYOffset <= pageYOffset) && (pageYOffset > 600)) {
       if (transform <= menuHeight) {
         transform = transform + (pageYOffset - lastPageYOffset)
@@ -30,23 +35,63 @@ $(document).ready(() => {
     }
 
     lastPageYOffset = pageYOffset
+  }
+
+  if (window.screen.width >= 690) {
+    window.addEventListener('scroll', scrollHandler)
+  }
+
+  window.addEventListener("resize", () => {
+    if (window.screen.width >= 690) {
+      window.addEventListener('scroll', scrollHandler)
+    } else {
+      window.removeEventListener('scroll', scrollHandler)
+    }
   })
 
   // menu
   $('.menu-btn').on('click', function (e) {
     e.preventDefault();
-    $('.main-menu').toggleClass('menu_active');
-    // document.body.style.overflow = 'hidden';
-
-
+    $('.menu').toggleClass('menu_active');
+    if ($('.menu').hasClass( "menu_active" )) {
+      disableBodyScroll();
+    }
   });
+
+  $('.overlay').on('click', function (e) {
+    $('.menu').toggleClass('menu_active');
+    if (!$('.menu').hasClass( "menu_active" )) {
+      enableBodyScroll();
+    }
+  })
+
+
+  // scroll to
+  let topOffset = 100
+
+  $('.menu-link').each(function() {
+    $(this).click(() => {
+      if (window.screen.width <= 690) {
+        enableBodyScroll();
+        $('.menu').toggleClass('menu_active');
+      }
+      
+      $(document.body).animate({
+        'scrollTop': $($(this).attr('href')).offset().top - topOffset
+      }, 500);
+    })
+  })
+
+  $('.keto-item').each(function() {
+    $(this).click(() => {
+      $(document.body).animate({
+        'scrollTop': $('#' + $(this).data().scroll).offset().top - topOffset
+      }, 500);
+    })
+  })
+  
+  $('.header-arrow__button').click(() => {
+    $('.header-easy')[0].scrollIntoView({block: "center", behavior: "smooth"})
+  })
+
 });
-
-
-
-
-
-//  document.onclick = function () {
-//    let container = $('.main-menu');
-//    container.removeClass('menu_active');
-//  }
